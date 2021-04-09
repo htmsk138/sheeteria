6155bc736a9c965d327da339e71a51bedccf9b48
Hitomi Seki <>
2021-04-09 17:30:56 +0900

    Rewrite main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,9 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
   sheets = document.querySelectorAll('#sheet-list li');
 });
 
+/**
+ * Filter sheet list on every keyup event on search box.
+ */
 function filterSheets(terms) {
-  var termList = terms.toLowerCase().split(' ');
+  var termList = terms.toLowerCase().split(' '), match = false, itemTitle;
+
   sheets.forEach(function(item) {
-    item.style.display = termList.every(function(term) { return item.innerText.split(' - ')[0].toLowerCase().includes(term) }) ? '' : 'none';
+    // Shorten target value to "Song Title (Artist Name)"
+    itemTitle = item.innerText.split(' - ')[0].toLowerCase();
+
+    // Test if the above contains every one of space-separated terms
+    match = termList.every(function(term) { return itemTitle.includes(term) });
+
+    // Display/hide the item depending on the test result
+    item.style.display = match ? '' : 'none';
   });
 }
